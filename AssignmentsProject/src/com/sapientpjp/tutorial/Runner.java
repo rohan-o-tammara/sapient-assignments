ef0ca7889d18a6cb2c7ec2ebcd536d3fd67822dc
Rohan Tammara <ta.rohan550@gmail.com>
2020-05-16 00:34:07 +0530

    timer update

--- AssignmentsProject/src/com/sapientpjp/tutorial/Runner.java
+++ AssignmentsProject/src/com/sapientpjp/tutorial/Runner.java
@@ -6,11 +6,15 @@ public class Runner {
 
         // Assignment 3 - Characters mapped to Prime Numbers to be converted to Hex
         CharacterToHexPrime charToHexPrime = new CharacterToHexPrime();
-        charToHexPrime.run();
+        //charToHexPrime.run();
 
         //Assignment 4 - Phrase translator
         Translator translator = new Translator();
         //translator.run();
+
+        //Assignment 5 - Quizzer
+        QuizMaster quiz = new QuizMaster();
+        quiz.begin();
     }
 
 }
